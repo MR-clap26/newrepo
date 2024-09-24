9b18994e092002c860414fbbba51e8aa5fba2f10
mr-clap26 <diego.art.bra@mail.com>
2024-09-24 16:27:43 -0300

    hool

--- Biblioteca/clases/Libro.py
+++ Biblioteca/clases/Libro.py
@@ -1,6 +1,6 @@
 class libro:
 
-    def _int_(self,ISBN,titulo,autor ):
-        self._ISBN=ISBN
+    def _int_(self,isbn,titulo,autor ):
+        self._isbn=isbn
         self._titulo=titulo
         self._autor=autor
---
+++ Biblioteca/clases/paises.py
@@ -0,0 +1,6 @@
+class pais:
+    def __init__(self) :
+        
+        
+        
+        pass
