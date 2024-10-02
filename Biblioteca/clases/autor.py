2e39efcbe9b57558c742a6396f848f5633653600
mr-clap26 <diego.art.bra@mail.com>
2024-10-02 17:44:37 -0300

    oka

---
+++ Biblioteca/clases/autor.py
@@ -0,0 +1,8 @@
+class autor:
+    def _init_(self,nombre,ap_paterno,ap_materno,fech_nac):
+        self.nombre=nombre
+        self.ap_paterno=ap_paterno
+        self.ap_materno=ap_materno
+        self.fech_nac=fech_nac
+
+    
---
+++ Biblioteca/clases/biblioteca.py
@@ -0,0 +1,7 @@
+class biblioteca:
+    def _init_(self,libros,personal,servicios,membresias):
+        self.libros=libros
+        self.personal=personal
+        self.servicios=servicios
+        self.membresias=membresias
+
