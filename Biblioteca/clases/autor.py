eea4c63e03e800397f6111a616692a3496c69db4
mr-clap26 <diego.art.bra@mail.com>
2024-10-08 16:22:08 -0300

    mono

--- Biblioteca/clases/autor.py
+++ Biblioteca/clases/autor.py
@@ -1,10 +1,10 @@
 class autor:
-    def _init_(self,id_autor,nombre,ap_paterno,ap_materno,fech_nac,pais):
+    def _init_(self,id_autor,nombre,ap_paterno,ap_materno,fech_nac,pais_origen):
         self.id_autor= id_autor
         self.nombre=nombre
         self.ap_paterno=ap_paterno
         self.ap_materno=ap_materno
         self.fech_nac=fech_nac
-        self.pais=pais
+        self.pais_origen=pais_origen
 
     
