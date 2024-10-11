842f0ff2ecd8ddbbf7905c00e22f2815256b6ae4
mr-clap26 <diego.art.bra@mail.com>
2024-10-10 21:19:16 -0300

    sss

---
+++ Biblioteca/clases/paises.py
@@ -0,0 +1,6 @@
+class paises :
+    def _init_(self,id_pais,nombre,gentilicio):
+        self.id_pais=id_pais
+        self.nombre=nombre
+        self.gentilicio=gentilicio
+    
--- Biblioteca/clases/usuario.py
+++ Biblioteca/clases/usuario.py
@@ -1,34 +1,20 @@
-class usuario:
-    def __init__(self,id_user,nombre,ap_pat,ap_mat,fecha_nac,num_telfono,correo,direccion,tipo_user):
-        self.id_user=id_user
-        self.nombre=nombre
-        self.ap_pat=ap_pat
-        self.ap_mat=ap_mat
-        self.fecha_nac=fecha_nac
-        self.num_telefono=num_telfono
-        self.correo=correo
-        self.direccion=direccion
-        self.tipo_user=tipo_user
-
-
 import paises
 import tipo_usuario
-
 from rut_chile import rut_chile
 
 class Usuario(paises, tipo_usuario):
-    def __init__(self, id_user, nombre, correo, telefono, rut,id_pais, habilitado, id_tipo_usuario, fecha_creacion):
+    def __init__(self, id_user, nombre, correo, telefono, rut , id_pais , id_tipo , fecha_crea):
         paises.__init__(id_pais)
-        tipo_usuario.__init__(id_tipo_usuario)
-        self.id_usuario = id_usuario
-        self.nombre_usuario = nombre_usuario
-        self.correo_usuario = correo_usuario
-        self.telefono_usuario = telefono_usuario
-        self.rut_usuario = rut_usuario
-        self.habilitado = habilitado
-        self.fecha_creacion = fecha_creacion
+        tipo_usuario.__init__(id_tipo)
+        self.id_user = id_user
+        self.nombre = nombre
+        self.correo= correo
+        self.telefono= telefono
+        self.rut = rut
+        self.fecha_crea = fecha_crea
     
     def validacion_rut(self):
         return rut_chile.is_valid_rut(self.rut_usuario)
     
     def validacion_correo(self):
+        pass
